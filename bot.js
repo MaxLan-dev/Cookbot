aa34f1c5eed8896ba34d79ba7e73c5e068b4bc36
Maksym Lan <lanmaksym@gmail.com>
2023-11-14 16:19:02 -0600

    Blah
    dd

---
+++ bot.js
@@ -0,0 +1,19 @@
+const TelegramBot = require('node-telegram-bot-api');
+const token = '6619295811:AAHobjQDD1uLuRMhNS6CeeMZtrfJs4ZRPt4'; // Replace with your own bot token
+const bot = new TelegramBot(token, { polling: true });
+bot.on('message', (msg) => {
+    const chatId = msg.chat.id;
+    const messageText = msg.text;
+    console.log(msg);
+    // Process the incoming message here
+  
+  });
+  bot.on('message', (msg) => {
+    const chatId = msg.chat.id;
+    const messageText = msg.text;
+  
+    if (messageText === '/start') {
+      bot.sendMessage(chatId, 'Welcome to the bot!');
+    }
+  });
+  
